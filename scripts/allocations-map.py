5ca88973825583f4c353b8c3c1c481d80ca9d4a9
eulisse <>
2009-11-16 17:05:25 +0000

    Improved version which reads the new file format.
    * Allows for highlighting a given stacktrace in the top 10.

--- scripts/allocations-map.py
+++ scripts/allocations-map.py
@@ -6,7 +6,6 @@
 # Each pixel in the image is a PAGESIZE page. Black means that the page is
 # unused, white it means that the page is completely used. Gray shades in 
 # between are for various degree of utilization.
-
 # Dimensions of the output image
 WIDTH=1024
 HEIGHT=1024
@@ -15,9 +14,10 @@ PAGESIZE=4096
 import sys
 import numpy
 from optparse import OptionParser
-from Image import fromarray
+from Image import fromarray, composite, blend
 from ImageDraw import ImageDraw
 import re
+from math import sqrt
 
 def die(*statements):
   for s in statements:
@@ -27,82 +27,162 @@ def die(*statements):
 USAGE="""Syntax: allocations -o <output-image> <input-filename>
 """
 
-# Returns true if the allocation fits in one single page.
-def singlePage(addr, size):
-  return (addr / PAGESIZE) == ((addr + size) / PAGESIZE)
+# Draws a given allocation at @a pos address of @a size bites in the memory
+# map @a data with a given allocation. The @a fill boolean can be used to
+# put a given value for each touched page.
+def paintAllocation(data, pos, size, fill=0):
+  startpage, endpage = pos/PAGESIZE, ((pos+size)/PAGESIZE)+1
+  if fill:
+    data[startpage:endpage] = fill
+    return
 
-# Add a contribution of size `size` to the page 
-def addToPixel(data, addr, size):
-  pageaddr = (addr / PAGESIZE)
-  x, y = (pageaddr % WIDTH), (pageaddr / HEIGHT)
-  data[x, y] += size
+  # If the allocation fits on a single page, just add the contribution of 
+  # that page.
+  if endpage-startpage == 1:
+    data[startpage] += size
+    return
 
-# Returns the lower / upper boundary of the page containing addr.
-def pageLowerBoundary(addr):
-  return (addr / PAGESIZE) * PAGESIZE
+  # If the allocation spans multiple pages, add the padding contribution
+  # and then fill the pages in between (excluding those used for the
+  # padding).
+  data[startpage] += PAGESIZE - (pos & (PAGESIZE-1))
+  data[endpage] += (pos+size) & (PAGESIZE-1)
+  data[startpage+1:endpage-1] = PAGESIZE
 
-def pageUpperBoundary(addr):
-  return ((addr / PAGESIZE) + 1) * PAGESIZE - 1 
+topTen = [[] for x in xrange(10)]
 
-if __name__ == "__main__":
+# Yields (node, symbol, pos, size) from a string of comma separated values.
+# In the case the line starts with @ fill in the top 10 list.
+def allocationInfo(l):
+  i = l.index(",")
+  j = i + 1 + l[i+1:].index(",")
+  k = j + 1 + l[j+1:].index(",")
+  return (int(l[:i], 16), int(l[i+1:j], 16), int(l[j+1:k], 16), int(l[k+1:], 16))
+
+def parseStacktrace(l):
+  parts = l.split(")", 1)
+  rank, level = parts[0][2:].split(",")
+  node, symbol = parts[1].split(":", 1)
+  topTen[int(rank)].append((int(node, 16), symbol))
+  return
+
+def paintData(data, allocations, nodeAllocations):
+  for node, symbol, pos, size in allocations:
+    # Accumulate cost for each call-tree node.
+    nodeAllocations[node].append((pos, size))
+    # Actually paint all the allocations on the map.
+    startpage, endpage = pos/PAGESIZE, ((pos+size)/PAGESIZE)
+    if startpage == endpage:
+      data[startpage] += size
+    else:
+      paintAllocation(data, pos, size)
+
+def saveImage(data, nodeAllocations, opts):
+  global WIDTH
+  global HEIGHT
+  global PAGESIZE
+
+  # Collapse "step" rows that are completely empty.
+  step = 4
+  collapser = numpy.zeros(WIDTH*HEIGHT, numpy.uint32)
+  for begin, end in ((WIDTH*y, WIDTH*y + WIDTH*step) 
+                     for y in xrange(0, HEIGHT, step)):
+    if any(data[begin:end]):
+      collapser[begin:end] = 1
+  data = data.compress(collapser)
+  
+  # Create the output false colors picture. Rescale the values in the
+  # data array so that they are normalised to 245 and convert the array
+  # to 8 bit so that it can be easily used with the paletted mode.
+  # Notice that we leave the last ten colors (245 - 255) to highlight
+  # fragmented memory allocations coming from a unique stacktrace node.
+  # Determine the 10 most fragmented allocations coming from the same symbol
+  # and highlight the pages they touch.
+  # Reshape the array to be bidimensional rather than linear.
+  data *= 245
+  data /= PAGESIZE
+  
+  # Calculate an highlight mask to hightlight top MEM_LIVE allocations.
+  highlightNode, highlighNodeName = topTen[4][0]
+  print highlighNodeName
+  highlightMask = numpy.zeros(WIDTH*HEIGHT, numpy.uint32)
+  for (pos, size) in nodeAllocations[highlightNode]:
+    paintAllocation(data, pos, size, 246)
+  highlightMask = highlightMask.compress(collapser)
+  
+  WIDTH = int(sqrt(len(data)))
+  data = numpy.reshape(data, (len(data)/WIDTH, WIDTH))
+  highlightMask = numpy.reshape(highlightMask, (len(highlightMask)/WIDTH, WIDTH))
+  HEIGHT, WIDTH = data.shape
+  
+  pilImage = fromarray(numpy.array(data, dtype=numpy.uint8), 'P')
+  highlightImage = fromarray(numpy.array(highlightMask, dtype=numpy.uint8), 'P')
+  maskImage = fromarray(numpy.array(highlightMask, dtype=numpy.uint8), '1')
+  lut = [0,0,0]
+  for x in (int(i / 245. * 255.) for i in range(245)):
+    lut.extend([x*0.8, x*0.8 , x*0.8])
+  # Extra colors are all purple.
+  for x in range(10):
+    lut.extend([186., 7., 17.])
+  
+  pilImage.putpalette(lut)
+#  highlightImage.putpalette(lut)
+  
+  d = ImageDraw(pilImage)
+  for c in xrange(256):
+    w, h = WIDTH*0.8 / 256, HEIGHT*0.01
+    x, y = c * w+WIDTH*0.1, HEIGHT*0.9 - h
+    d.rectangle((x, y, x+w, y+h), fill=c)
+#  pilImage = composite(pilImage, highlightImage, maskImage)
+  pilImage = pilImage.resize((WIDTH*2, HEIGHT*2))
+  pilImage.save(opts.output)
+
+
+def main():
+  global WIDTH
+  global HEIGHT
+  global PAGESIZE
   parser = OptionParser()
   parser.add_option("-o", "--output", dest="output", 
                     default="allocations-map.png")
+  parser.add_option("--debug", dest="debug", action="store_true")
+  parser.add_option("--entry", dest="entry")
   opts, args = parser.parse_args()
 
   if len(args) != 1:
     die(USAGE)
 
   # Create an image, parse the output using a regexp.
+  # Accumulate per node information to decide which are the nodes
+  # to be highlighted and which not.
   # Split large allocations in pages of PAGESIZE bytes and
   # add contributions to each page accordingly.
-  data = numpy.zeros((WIDTH,HEIGHT), numpy.uint32)
+  # The allocations list contains elements of the following form:
+  #
+  # node, symbol, pos, size
+  #
+  data = numpy.zeros(WIDTH*HEIGHT, numpy.uint32)
+  print "Reading file %s" % args[0]
+  f = file(args[0], "r")
+  allocations = []
+  lines = [l for l in f.readlines()]
+  print "Done reading"
+  allocations = [allocationInfo(l) for l in lines if l[0] != "@"]
+  [parseStacktrace(l) for l in lines if l[0] == "@"]
+  
+  # Map which keeps track of the total amount of memory allocated per node.
+  nodeAllocations = dict((info[0], []) for info in allocations)
 
-  lineRE = re.compile("([^,]*),([^,]*):([^,]*),([^,]*)")
+  print "Painting"
+  paintData(data, allocations, nodeAllocations)
 
-  for l in file(sys.argv[1], "r").readlines():
-    node, symbol, pos, size = lineRE.match(l).groups()
-    size = int(size)
-    pos = int(pos, 16)
-    
-    # If the allocation fits on a single page, just add the contribution of 
-    # that page. 
-    if singlePage(pos, size):
-      addToPixel(data, pos, size)
-      continue
-    
-    # If the allocation spans multiple pages, add the padding contribution
-    # and then fill the pages in between (excluding those used for the 
-    # padding, hence the +1 -1 in the loop).
-    paddingBefore = pageUpperBoundary(pos) - pos
-    addToPixel(data, pos, paddingBefore)
-    paddingAfter = (pos + size) - pageLowerBoundary(pos + size)
-    addToPixel(data, pos + size, paddingAfter)
-    
-    firstFullPageAddr = ((pos / PAGESIZE) + 1) * PAGESIZE
-    lastFullPageAddr = (((pos + size) / PAGESIZE) - 1) * PAGESIZE
-    try: 
-      for x in xrange(firstFullPageAddr,
-                      lastFullPageAddr, 
-                      PAGESIZE):
-        addToPixel(data, x, PAGESIZE)
-    except OverflowError, e:
-      print firstFullPageAddr, lastFullPageAddr
-  # Create the output grayscale picture. Rescale the values in the
-  # data array so that they are normalised to 255 and convert the array
-  # two 8 bit so that it can be easily used with the paletted mode.
-  data *= 255
-  data /= PAGESIZE
-  data = numpy.transpose(data)
-      
-  pilImage = fromarray(numpy.array(data, dtype=numpy.uint8), 'P')
-  lut = [0,0,0]
-  for x in range(255):
-    lut.extend([255-x,x,x/2])
-  pilImage.putpalette(lut)
-  d = ImageDraw(pilImage)
-  for c in xrange(256):
-    w, h = WIDTH*0.8 / 256, HEIGHT*0.01
-    x, y = c * w+WIDTH*0.1, HEIGHT*0.9 - h
-    d.rectangle((x, y, x+w, y+h), fill=c)
-  pilImage.save(opts.output)
+  print "Saving image"
+  saveImage(data, nodeAllocations, opts)
+
+import cProfile
+
+if __name__ == "__main__":
+  if "--debug" in sys.argv:
+    cProfile.run('main()')
+  else:
+    main()
